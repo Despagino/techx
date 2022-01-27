a444a662f95bfa3b815918f4e0170dacb3a44d70
Gino Tasis <gtasis84@gmail.com>
2022-01-27 18:21:21 -0500

    Add

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require("cors");
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+
+app.use("/styles", express.static(path.join(__dirname, "../client/reset.css")));
+
+app.use("/js", express.static(path.join(__dirname, "../client/app.js")));
 
 
 const port = process.env.PORT || 4005
