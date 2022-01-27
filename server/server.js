7c236ef094cc28d50e9ec4ebdd435ded5fb1238f
Gino Tasis <gtasis84@gmail.com>
2022-01-27 11:51:45 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express')
-require('dotenv').config()
 const path = require('path')
-const port = process.env.PORT
+
 
 const app = express()
 
 app.use(express.json())
 
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/index.html'))
+    res.sendFile(path.join(__dirname, './client/index.html'))
 })
 
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
+const port = process.env.PORT || 4005
 
 app.listen(port, () => {
     console.log("Listening on port " + port)
